81d6bb4313ae10e8f3d50ee31f4ff33243f77539
seeun2150 <seeun2150@gachon.ac.kr>
2025-11-15 23:48:58 +0900

    Add tester.py for geometry utility functions

---
+++ geo/geo/tester.py
@@ -0,0 +1,10 @@
+import geo.utils as utils
+
+a, b = 3, 4
+c = utils.pythagoras(a, b)
+print('c =', c)
+
+# calculate the area of circle with radius r = 10
+r = 10
+area = utils.circle(r)
+print('area =', area)
